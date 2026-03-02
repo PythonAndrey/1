12b3c4011b637311f0b76d7c7319f4daf1933681
Home45 <77k7home@mail.ru>
2026-03-03 02:23:48 +0600

    HelloWorld создание файла

--- src/main/java/org/javaguru/JavaAndrey/HelloWorld.java
+++ src/main/java/org/javaguru/JavaAndrey/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Valera");
-        System.out.println("Kovs");
+        System.out.println("Kovs ");
+
     }
 }
