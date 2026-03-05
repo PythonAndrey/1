e5c59b7147cf18cf9a11c1a4a240078f2d1ac523
Home45 <77k7home@mail.ru>
2026-03-05 17:13:51 +0600

    Создание ещё одного конструктора в классе BankAccount с одним параметром

--- src/main/java/org/javaguru/JavaAndrey/lesson3oop/BankAccountApplication.java
+++ src/main/java/org/javaguru/JavaAndrey/lesson3oop/BankAccountApplication.java
@@ -5,7 +5,7 @@ class BankAccountApplication {
         BankAccount bankAccount1 = new BankAccount("Андрей", 1);
         BankAccount bankAccount2 = new BankAccount("Оксана", 2);
 
-        System.out.println(bankAccount1 + "\n" + bankAccount2);
+        BankAccount bankAccountOwner1 = new BankAccount("Марина");
 
     }
 }
