ad376dc1fd29070b8099a3b60fc9dd93c187655c
Home45 <77k7home@mail.ru>
2026-03-05 17:28:07 +0600

    Добавил в конструктор нулевое значение параметра money

--- src/main/java/org/javaguru/JavaAndrey/lesson3oop/BankAccount.java
+++ src/main/java/org/javaguru/JavaAndrey/lesson3oop/BankAccount.java
@@ -7,11 +7,13 @@ class BankAccount {
 
     public BankAccount(String owner){
         this.owner = owner;
+        this.money = 0;
     }
 
     public BankAccount(String owner, int money){
         this.owner = owner;
         this.money = money;
     }
+
 }
 
