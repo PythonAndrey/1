ea30b159e7fb9bd7b71a082e633ccb37661ab96b
Home45 <77k7home@mail.ru>
2026-03-06 10:05:16 +0600

    Добавил Laptop.java и LaptopApp.java

---
+++ src/main/java/org/javaguru/JavaAndrey/lesson3oop/Laptop.java
@@ -0,0 +1,14 @@
+package org.javaguru.JavaAndrey.lesson3oop;
+
+class Laptop {
+
+    // Объекты: model - модель, price - цена
+    private String model;
+    private int price;
+
+    //Конструктор начальных значений для свойств создаваемого объекта Ноутбук.
+    public Laptop(String model, int price){
+        this.model = model;
+        this.price = price;
+    }
+}
---
+++ src/main/java/org/javaguru/JavaAndrey/lesson3oop/LaptopApp.java
@@ -0,0 +1,7 @@
+package org.javaguru.JavaAndrey.lesson3oop;
+
+public class LaptopApp {
+    public static void main(String[] args) {
+
+    }
+}
